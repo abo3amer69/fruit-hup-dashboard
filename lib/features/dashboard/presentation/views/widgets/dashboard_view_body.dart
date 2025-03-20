7da956af09eb831d727d948947cb9df5a891d65f
Ahmed Amer <01099517574med@gmail.com>
2025-03-20 18:04:44 +0200

    create add product view

--- lib/features/dashboard/presentation/views/widgets/dashboard_view_body.dart
+++ lib/features/dashboard/presentation/views/widgets/dashboard_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hup_dashboard/core/widgets/custom_button.dart';
+import 'package:fruit_hup_dashboard/features/add_product/presentation/views/add_product_view.dart';
 
 class DashboardViewBody extends StatelessWidget {
   const DashboardViewBody({super.key});
@@ -10,7 +11,14 @@ class DashboardViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [CustomButton(onPressed: () {}, text: 'Add Data')],
+        children: [
+          CustomButton(
+            onPressed: () {
+              Navigator.pushNamed(context, AddProductView.routeName);
+            },
+            text: 'Add Data',
+          ),
+        ],
       ),
     );
   }
