f5882154f5ba2601d40ebd22a048a8998d6a9d1b
Ahmed Amer <01099517574med@gmail.com>
2025-03-22 02:08:16 +0200

    add package image picker and package skeltonizer

--- lib/core/widgets/image_field.dart
+++ lib/core/widgets/image_field.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
-class ImageField extends StatelessWidget {
+class ImageField extends StatefulWidget {
   const ImageField({super.key});
 
   @override
+  State<ImageField> createState() => _ImageFieldState();
+}
+
+class _ImageFieldState extends State<ImageField> {
+  bool isLoading = false;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(color: Colors.grey),
+    return Skeletonizer(
+      enabled: isLoading,
+      child: GestureDetector(
+        onTap: () async {
+          isLoading = true;
+          setState(() {});
+          try {
+            final ImagePicker picker = ImagePicker();
+
+            final XFile? image = await picker.pickImage(
+              source: ImageSource.gallery,
+            );
+          } on Exception catch (e) {
+            // TODO
+          }
+          isLoading = false;
+          setState(() {});
+        },
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: Colors.grey),
+          ),
+          child: Icon(Icons.image_outlined, size: 180),
+        ),
       ),
-      child: Icon(Icons.image_outlined, size: 180),
     );
   }
 }
